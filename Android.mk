584968f5db83fea6df333e1558855f56cd12d5b0
Borja Díaz de Terán <borja@diazdeteran.com>
2016-03-19 03:31:51 -0400

    Adds libupm to maker file

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hello_led_service
 LOCAL_SRC_FILES := hello_led_service.cpp
-LOCAL_SHARED_LIBRARIES := libc libbase libmraa
+LOCAL_SHARED_LIBRARIES := libc libbase libmraa libupm
 LOCAL_CFLAGS := -Werror
 include $(BUILD_EXECUTABLE)
